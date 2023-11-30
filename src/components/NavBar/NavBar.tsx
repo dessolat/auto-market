48d1438b9224a72637bb590c05df1afec14f724f
Igor Nikiforov <desola2@inbox.ru>
2023-11-30 17:13:30 +0300

    feat: render links from array

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 import cl from './NavBar.module.scss';
+import { linksType } from './types';
 
 const NavBar = () => {
+  const links: linksType = [{ label: 'All Cars', href: '/' }];
+
   return (
     <nav className={cl.nav}>
       <ul className={cl.navList}>
-        <li className={cl.listItem}>
-					<Link href='/'>All cars</Link>
-        </li>
+        {links.map(link => (
+          <li key={link.href} className={cl.listItem}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
